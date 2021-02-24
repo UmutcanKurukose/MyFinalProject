42f49101b93ee946a10dd735a0f5f61950651e49
simurg12 <65874751+simurg12@users.noreply.github.com>
2021-02-24 05:21:53 +0300

    24 Şubat 2021 Ders Tekrarı

---
+++ Business/Constant/Messages.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constant
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string ProductNameInvalid = "Ürünn adı geçersiz";
+        public static string MaintenanceTime ="Sistem bakımda";
+        public static string ProductListed = "ürünler listelendi";
+    }
+}
